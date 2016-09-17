012f2732462d8ce4db257f4a955b60eb9cf472a7
Stautob <stautob@stautob.ch>
2016-09-17 13:33:08 +0200

    Complete redesign

--- app/src/main/java/ch/clubm8/clubm8/MainActivity.java
+++ app/src/main/java/ch/clubm8/clubm8/MainActivity.java
@@ -80,15 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_news) {
+        if (id == R.id.nav_camera) {
             // Handle the camera action
-        } else if (id == R.id.nav_schedule) {
+        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_events) {
+        } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.login) {
+        } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_settings) {
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
 
         }
 
