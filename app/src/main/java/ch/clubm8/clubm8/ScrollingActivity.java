b7c0a490e5758949d325ee86bcd8cb89e0d2a878
Stautob <stautob@stautob.ch>
2016-09-17 11:28:39 +0200

    Fixes

--- app/src/main/java/ch/clubm8/clubm8/ScrollingActivity.java
+++ app/src/main/java/ch/clubm8/clubm8/ScrollingActivity.java
@@ -14,7 +14,7 @@ public class ScrollingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scrolling);
+        setContentView(R.layout.activity_news);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
