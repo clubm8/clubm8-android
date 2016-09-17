34053d24abf8ff2df612dae9fbec53158e009eaf
Ueli Bosshard <ueli@lilo.ch>
2016-09-17 19:53:12 +0200

    DbHelper now has a news table

--- app/src/main/java/ch/clubm8/clubm8/helper/myDbHelper.java
+++ app/src/main/java/ch/clubm8/clubm8/helper/myDbHelper.java
@@ -7,18 +7,16 @@ import android.util.Log;
 
 public class MyDbHelper extends SQLiteOpenHelper {
 
-    public static final String TABLE_COMMENTS = "comments";
-    public static final String COLUMN_ID = "_id";
-    public static final String COLUMN_COMMENT = "comment";
-
-    private static final String DATABASE_NAME = "commments.db";
+    private static final String DATABASE_NAME = "clubm8.db";
     private static final int DATABASE_VERSION = 1;
 
     // Database creation sql statement
     private static final String DATABASE_CREATE = "create table "
-            + TABLE_COMMENTS + "( " + COLUMN_ID
-            + " integer primary key autoincrement, " + COLUMN_COMMENT
-            + " text not null);";
+            + "news " + "( " + "id" + " integer primary key autoincrement, "
+            + "date" + " text not null, "
+            + "time" + " text not null, "
+            + "title" + " text not null, "
+            + "text" + " text not null);";
 
     public MyDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -34,7 +32,7 @@ public class MyDbHelper extends SQLiteOpenHelper {
         Log.w(MyDbHelper.class.getName(),
                 "Upgrading database from version " + oldVersion + " to "
                         + newVersion + ", which will destroy all old data");
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_COMMENTS);
+        db.execSQL("DROP TABLE IF EXISTS " + "news");
         onCreate(db);
     }
 
